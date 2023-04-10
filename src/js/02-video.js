8470ee130e3dd031b22ae529d317f3d50fe51f49
Anastasiia Akhmedkhanova <nastydoc@ukr.net>
2023-04-10 19:39:46 +0300

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from "@vimeo/player";
+import _throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const videoCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(videoCurrentTime || '0');
+player.on('timeupdate', _throttle(({ seconds }) =>
+    localStorage.setItem('videoplayer-current-time', seconds), 1000)); 
+    //     console.log('played the video!');
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import _throttle from 'lodash.throttle';
+
+const feedbackFormRef = document.querySelector('.feedback-form');
+feedbackFormRef.addEventListener('input', _throttle(onFormInputText, 500));
+feedbackFormRef.addEventListener('submit', onFormSubmit);
+
+const SAVE_KEY = "feedback-form-state";
+let formData = {
+    email: '',
+    message: ''
+};
+
+startPage()
+
+function onFormInputText(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(SAVE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    if (formData.email === '' || formData.message === '') {
+        alert('Заповніть поля!')
+        return
+    }
+    console.log(formData)
+
+    Object.keys(formData).forEach(key => (formData[key] = ''));
+
+    event.currentTarget.reset();
+    localStorage.removeItem(SAVE_KEY)
+};
+
+function startPage(event) {
+    const saveMessage = localStorage.getItem(SAVE_KEY);
+    const stringMessage = JSON.parse(saveMessage);
+
+    for (const key in stringMessage) {
+        feedbackFormRef.elements[key].value = stringMessage[key];
+        formData[key] = stringMessage[key];
+    }
+
+};
 
