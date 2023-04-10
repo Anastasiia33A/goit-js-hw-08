e2f495224381954b52bc854ba5b4d7460966e76a
Anastasiia Akhmedkhanova <nastydoc@ukr.net>
2023-04-10 17:40:24 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image"
+            src="${preview}" data-source="${original}" alt="${description}"/>
+            </a>
+        </li>`
+)
+    .join('');
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup)
+    
+const galleryBox = new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: '100',
+    navText: ['⇦', '⇨']
+
+});
+// console.log(galleryItems);
